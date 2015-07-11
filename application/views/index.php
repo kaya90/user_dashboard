2807f340150065c62def4a3aef1f50cdb9e7a04d
Kaya Kim <9474ing.rain@gmail.com>
2015-07-11 10:21:59 -0700

    setting up master

---
+++ application/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+</head>
+<body>
+Here
+</body>
+</html>
